b1d547f08385084e5e03fb3cb1645479b9f81585
Robinson <robinsonclaveria1993@gmail.com>
2022-06-14 15:11:00 -0400

    crossOrigin

--- src/main/java/com/Inventario/controlador/CategoriaRestController.java
+++ src/main/java/com/Inventario/controlador/CategoriaRestController.java
@@ -2,6 +2,7 @@ package com.Inventario.controlador;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.Inventario.model.Categoria;
 import com.Inventario.response.CategoriaResponseRest;
 import com.Inventario.servicio.ICategoriaServicio;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/v1")
 public class CategoriaRestController {
